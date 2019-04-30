8fc009c103a07f1a3b997026a8d13ce8a64597f5
Shilza <andgalatinov@gmail.com>
2019-04-30 22:19:27 +0300

    Add canvas waveform template

--- src/index.js
+++ src/index.js
@@ -24,3 +24,23 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// waveform_url https://wis.sndcdn.com/2Z2RqPqtmtmD_m.json
+// let canvas = document.getElementById('canvas');
+// canvas.height = data.height / 2; // 70px;
+// canvas.width = data.width / 2;   // 900px
+//
+// let context = canvas.getContext('2d');
+// context.fillStyle = '#1d1e1f';
+//
+// let samples = data.samples,
+//     l = samples.length,
+//     i = 0,
+//     x = 0,
+//     v;
+//
+// for (; i < l; i += 2, x++) {
+//     v = samples[i] / 4;
+//     context.fillRect(x, 0, 1, 35 - v);
+//     context.fillRect(x, 35 + v, 1, 70);
+// }
