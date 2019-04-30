5bf9b416894c2c0586e7b0c7cb8d2d20dc93ddd5
Shilza <andgalatinov@gmail.com>
2019-04-30 22:18:46 +0300

    Add stop player middleware

--- src/store.js
+++ src/store.js
@@ -5,14 +5,14 @@ import { all } from 'redux-saga/effects';
 import { favoritesSaga } from "./features/favorites/sagas";
 import {trackInterval} from "./features/favorites/middlewares/trackInterval";
 import {seek} from "./features/favorites/middlewares/seek";
+import {stopPlayer} from "./features/favorites/middlewares/stopPlayer";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     RootReducer,
-    applyMiddleware(sagaMiddleware, trackInterval, seek)
+    applyMiddleware(sagaMiddleware, trackInterval, seek, stopPlayer)
 );
-
 
 function* rootSaga() {
     yield all([
