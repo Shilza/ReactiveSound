b50bd33eb590de98c4ddb01785f4d1a8eb458457
Shilza <andgalatinov@gmail.com>
2019-04-30 22:12:47 +0300

    Header fixes

--- src/features/common/organisms/Header/index.js
+++ src/features/common/organisms/Header/index.js
@@ -1,11 +1,23 @@
-import React from "react";
-import { Navigation } from '../../molecules';
-import { Name } from '../../molecules';
+import React, {useState} from "react";
+import {Navigation} from '../../molecules';
+import {Name} from '../../molecules';
 import styles from './styles.module.scss';
+import {SearchBar} from "../SearchBar";
 
-export const Header = () => (
-    <header className={styles.header}>
-        <Name/>
-        <Navigation/>
-    </header>
-);
+export const Header = () => {
+    let [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+
+    const onSearch = () => {
+        setIsSearchBarVisible(!isSearchBarVisible);
+    };
+
+    return (
+        <header className={styles.container}>
+            <div className={styles.header}>
+                <Name/>
+                <Navigation onSearch={onSearch}/>
+            </div>
+            <SearchBar visible={isSearchBarVisible}/>
+        </header>
+    );
+};
