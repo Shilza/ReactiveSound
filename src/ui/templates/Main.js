2bff6f07b58febd7b86b486ae6318399ba070d72
Shilza <andgalatinov@gmail.com>
2019-05-01 22:40:35 +0300

    Add SongBar to Main component

--- src/ui/templates/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Main = ({ header, children }) => (
-    <div>
-        {header}
-        <main>{children}</main>
-    </div>
-);
--- src/ui/templates/index.js
+++ src/ui/templates/index.js
@@ -1,2 +1,2 @@
 
-export { Main } from './Main';
+export { Main } from './Main/Main';
