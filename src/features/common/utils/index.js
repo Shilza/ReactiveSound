3d59848a8ae4a293cb863a8533814305428783da
Shilza <andgalatinov@gmail.com>
2019-05-02 21:04:00 +0300

    Fix utils

--- src/features/common/utils/index.js
+++ src/features/common/utils/index.js
@@ -1,11 +1,11 @@
 
-const convertMsToString = ms => {
+const convertMsToString = (ms = 0) => {
     let minutes = Math.floor(ms / (1000 * 60));
     let seconds = Math.floor((ms - minutes * 1000 * 60) / 1000);
     return minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 };
 
-const convertStringToMs = string => {
+const convertStringToMs = (string = '') => {
     let minutes = parseInt(string.substr(0, 2));
     let seconds = parseInt(string.substr(3, 5));
     return minutes * 1000 * 60 + seconds * 1000;
