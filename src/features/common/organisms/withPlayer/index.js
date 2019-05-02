245191fe06f39d18fcc612473c3f08cc70897181
Shilza <andgalatinov@gmail.com>
2019-05-02 21:12:12 +0300

    Fix withPlayer HOC

---
+++ src/features/common/organisms/withPlayer/index.js
@@ -0,0 +1,47 @@
+import {fetchTrack, pauseTrack, playTrack} from "../../../favorites/actionCreators";
+import {useCallback, useEffect} from "react";
+import hoistNonReactStatics from "hoist-non-react-statics";
+import React from "react";
+import {connect} from "react-redux";
+
+export const withPlayer = WrappedComponent => {
+
+    const PlayerWrapper = connect(mapStateToProps)(({dispatch, player, currentTrackId, trackIntervalId, id, ...props}) => {
+        useEffect(() => {
+            if (player && currentTrackId === id) {
+                dispatch(playTrack());
+            }
+        }, [player, dispatch, currentTrackId, id]);
+
+        const onPlay = useCallback(() => {
+            if (player && currentTrackId === id)
+                player.isPlaying() ? dispatch(pauseTrack()) : dispatch(playTrack());
+            else
+                dispatch(fetchTrack(id));
+        }, [player, dispatch, currentTrackId, id]);
+
+        let isPlay = !Object.is(trackIntervalId, null) && currentTrackId === id;
+
+        return <WrappedComponent {...props} onPlay={onPlay} isPlay={isPlay}/>
+    });
+
+    hoistNonReactStatics(PlayerWrapper, WrappedComponent);
+
+    PlayerWrapper.displayName = `PlayerWrapper(${getDisplayName(
+        WrappedComponent
+    )})`;
+
+    return PlayerWrapper;
+};
+
+const mapStateToProps = state => ({
+    player: state.favorite.player.player,
+    currentTrackId: state.favorite.player.currentTrack && state.favorite.player.currentTrack.id,
+    trackIntervalId: state.favorite.player.trackIntervalId
+});
+
+const getDisplayName = WrappedComponent => {
+    return (
+        WrappedComponent.displayName || WrappedComponent.name || "PlayerWrapper"
+    );
+};
