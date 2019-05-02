7cd0f1bc1f1ad133c71ea2963fc979483a20ad65
Shilza <andgalatinov@gmail.com>
2019-05-02 21:04:18 +0300

    Stop player middleware fixes

--- src/features/favorites/middlewares/stopPlayer.js
+++ src/features/favorites/middlewares/stopPlayer.js
@@ -1,10 +1,11 @@
 import {SET_PLAYER} from "../actionTypes";
-import {setPlayerInterval} from "../actionCreators";
+import {setCurrentTime, setPlayerInterval} from "../actionCreators";
 
 export const stopPlayer = store => next => action => {
     if(action.type === SET_PLAYER && store.getState().favorite.player.player) {
         store.getState().favorite.player.player.pause();
         clearInterval(store.getState().favorite.player.trackIntervalId);
+        store.dispatch(setCurrentTime(0));
         store.dispatch(setPlayerInterval(undefined));
     }
     next(action);
