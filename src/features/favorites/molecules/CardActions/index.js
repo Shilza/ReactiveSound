28510d44c02f2642af19c7931f88c4fe6405885b
Shilza <andgalatinov@gmail.com>
2019-05-02 21:06:05 +0300

    Song card fixes

--- src/features/favorites/molecules/CardActions/index.js
+++ src/features/favorites/molecules/CardActions/index.js
@@ -1,43 +1,12 @@
-import React, {useCallback, useEffect} from "react";
+import React from "react";
 import styles from './styles.module.scss';
 import {Button, Icon} from "../../../../ui/atoms";
-import {connect} from "react-redux";
-import {fetchTrack, pauseTrack, playTrack} from "../../actionCreators";
+import {PlayButton} from "../../../common/atoms";
 
-export const CardActions = ({id, link, title, trackIntervalId, currentTrackId, duration, dispatch, player}) => {
-
-    useEffect(() => {
-        if(player && currentTrackId === id) {
-            dispatch(playTrack());
-        }
-    }, [player, dispatch, currentTrackId, id]);
-
-    const onPlay = useCallback(() => {
-        if (player && currentTrackId === id)
-            player.isPlaying() ? dispatch(pauseTrack()) : dispatch(playTrack());
-        else
-            dispatch(fetchTrack({
-                id, duration, title
-            }));
-    }, [player, dispatch, currentTrackId, id, duration, title]);
-
-    let isPlay = !Object.is(trackIntervalId, null) && currentTrackId === id;
-    return (
-        <Body link={link} duration={duration} isPlay={isPlay} onPlay={onPlay}/>
-    );
-};
-
-const Body = React.memo(({onPlay, duration, isPlay, link}) => (
+export const CardActions = ({id, link, duration}) => (
     <div className={styles.container}>
         <div className={styles.playContainer}>
-            <Button onClick={onPlay}>
-                {
-                    isPlay ?
-                        <Icon name='pause' viewBox='0 0 420 420' fill='#4d4e4f'/>
-                        :
-                        <Icon name='play' viewBox='0 0 420 420' fill='#4d4e4f'/>
-                }
-            </Button>
+            <PlayButton id={id}/>
             <span>{duration}</span>
         </div>
         <a href={link} target='_blank' rel='noopener noreferrer'>
@@ -46,10 +15,4 @@ const Body = React.memo(({onPlay, duration, isPlay, link}) => (
             </Button>
         </a>
     </div>
-));
-
-export default connect(state => ({
-    player: state.favorite.player.player,
-    currentTrackId: state.favorite.player.currentTrack && state.favorite.player.currentTrack.id,
-    trackIntervalId: state.favorite.player.trackIntervalId
-}))(CardActions);
+);
--- src/features/favorites/organisms/CardBody/index.js
+++ src/features/favorites/organisms/CardBody/index.js
@@ -2,20 +2,18 @@ import React from "react";
 import styles from './styles.module.scss';
 import {CardActions} from "../../molecules/";
 import {TimeLine} from '../../../common/atoms'
+import {Link} from "react-router-dom";
 
-export const CardBody = ({currentTrackId, src, id, link, username, title, cover, duration}) => (
+export const CardBody = ({currentTrackId,userId,  src, id, link, username, title, cover, duration}) => (
     <>
         <div className={styles.imageContainer}>
             <img className={styles.cover} src={cover} alt='cover'/>
             {
-                currentTrackId === id &&
-                    <TimeLine/>
+                currentTrackId === id && <TimeLine id={id}/>
             }
         </div>
         <div className={styles.info}>
-            <div className={styles.username}>
-                <a href={'/'}>{username}</a>
-            </div>
+            <Link to={`/users/${userId}/tracks`} className={styles.username}>{username}</Link>
             <span className={styles.title}>{title}</span>
             <CardActions id={id} duration={duration} link={link} title={title}/>
         </div>
