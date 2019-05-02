ef0e667068782d2b3a40a347b640de9286128ca6
Shilza <andgalatinov@gmail.com>
2019-05-02 20:59:53 +0300

    TimeLine changes

--- src/features/common/atoms/TimeLIne/index.js
+++ src/features/common/atoms/TimeLIne/index.js
@@ -1,32 +1,12 @@
-import React, {useMemo, useRef} from "react";
+import React from "react";
 import styles from './styles.module.scss';
-import {connect} from "react-redux";
-import {convertStringToMs} from "../../utils";
-import {seekTo} from "../../../favorites/actionCreators";
+import {withTimeLine} from "../wthTimeLine";
 
-const TimeLine = ({currentTime, duration, dispatch}) => {
-    let timeLine = useRef();
-
-    let memoizedDuration = useMemo(() =>
-        convertStringToMs(duration), [duration]);
-
-    const seek = event => {
-            let width = getComputedStyle(timeLine.current).width;
-            let percents = event.nativeEvent.offsetX / parseInt(width) * 100;
-            let seekMs = memoizedDuration / 100 * percents;
-            dispatch(seekTo(seekMs));
-    };
-
-    return (
-        <div ref={timeLine} className={styles.timeLine} onClick={seek}>
-            <div className={styles.currentTime}
-                 style={{width: `${currentTime / memoizedDuration * 100}%`}}>
-            </div>
+export const TimeLine = withTimeLine(({currentTimeLinePosition}) => (
+    <div className={styles.timeLine}>
+        <div className={styles.currentTime}
+             style={{width: currentTimeLinePosition}}>
         </div>
-    );
-};
+    </div>
+));
 
-export default connect(state => ({
-    currentTime: state.favorite.player.currentTime,
-    duration: state.favorite.player.currentTrack.duration
-}))(TimeLine);
