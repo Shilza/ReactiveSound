8e3a0276557589bf1ffa50200e2a26e7900cc187
Shilza <andgalatinov@gmail.com>
2019-05-05 22:45:02 +0300

    Add pagination hoc

--- src/features/common/atoms/index.js
+++ src/features/common/atoms/index.js
@@ -3,4 +3,5 @@ import PlayerTime from './PlayerTime';
 export { PlayButton } from './PlayButton';
 export { TimeLine } from './TimeLIne';
 export { withTimeLine } from './wthTimeLine';
+export { withPagination } from './withPagination';
 export { PlayerTime };
