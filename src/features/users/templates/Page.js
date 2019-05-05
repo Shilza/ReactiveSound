e452f5f011ad84f55a1fc2cf0840f7b91070db6c
Shilza <andgalatinov@gmail.com>
2019-05-05 22:56:10 +0300

    Add Page template

---
+++ src/features/users/templates/Page.js
@@ -0,0 +1,27 @@
+import React, {useCallback, useEffect} from "react";
+import {CommonContent} from "../../common/templates";
+import {SubHeader} from "../../favorites/organisms/SubHeader";
+import {AdaptiveTracks} from "../../../ui/templates";
+import {Stats} from "../organisms";
+import {fetchUser} from "../actionCreators";
+
+export const Page = ({nextPage, dispatch, id, loading, tracks, tracksFethcer, tracksFethcerByPage, user}) => {
+
+    useEffect(() => {
+        dispatch(fetchUser(id));
+        dispatch(tracksFethcer(id));
+    }, [id, tracksFethcer, dispatch]);
+
+    const fetchNext = useCallback(() => {
+        dispatch(tracksFethcerByPage(id));
+    }, [id, tracksFethcerByPage, dispatch]);
+
+    return (
+        <CommonContent>
+            <SubHeader location={user && user.username}>
+                <Stats loading={true} user={user}/>
+            </SubHeader>
+            <AdaptiveTracks loading={loading} hasMore={typeof nextPage === 'string'} fetchNext={fetchNext} tracks={tracks}/>
+        </CommonContent>
+    );
+};
