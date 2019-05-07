a704a5c20df2e0950db6f898a9c34e115b799c7e
Shilza <andgalatinov@gmail.com>
2019-05-07 22:06:46 +0300

    Move to common & optimize SubHeader

--- src/features/common/organisms/SubHeader/index.js
+++ src/features/common/organisms/SubHeader/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from './subHeader.module.scss'
 
-export const SubHeader = ({section, location, children}) => (
+export const SubHeader = React.memo(({section, location, children}) => (
     <div className={styles.subHeader}>
         <div className={styles.container}>
             {
                 section && <div className={styles.section}>{section} /</div>
             }
-            <h2 className={styles.title}>{location}</h2>
+            <h1 className={styles.title}>{location}</h1>
             {children}
         </div>
     </div>
-);
+));
