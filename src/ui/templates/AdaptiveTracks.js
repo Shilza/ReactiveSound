cb662b0877d70595c80f53060f2502a0822b0fbe
Shilza <andgalatinov@gmail.com>
2019-05-07 22:10:36 +0300

    Replace withLoader for AdaptiveTracks component & add React.memo optimizer

--- src/ui/templates/AdaptiveTracks.js
+++ src/ui/templates/AdaptiveTracks.js
@@ -2,14 +2,14 @@ import React from "react";
 import MediaQuery from "react-responsive";
 import {WaveformTracks} from "./WaveformTracks";
 import {Tracks} from "../../features/favorites/templates";
-import {withLoader} from "../../features/common/organisms/withLoader";
 
-export const AdaptiveTracks = withLoader(({tracks, hasMore, fetchNext}) => (
-    <MediaQuery minWidth={960}>
-        {(matches) =>
-            matches
-                ? <WaveformTracks hasMore={hasMore} fetchNext={fetchNext} tracks={tracks}/>
-                : <Tracks hasMore={hasMore} fetchNext={fetchNext} tracks={tracks}/>
-        }
-    </MediaQuery>
-));
+export const AdaptiveTracks = React.memo(({tracks, hasMore, fetchNext}) => (
+        <MediaQuery minWidth={960}>
+            {(matches) =>
+                matches
+                    ? <WaveformTracks hasMore={hasMore} fetchNext={fetchNext} tracks={tracks}/>
+                    : <Tracks hasMore={hasMore} fetchNext={fetchNext} tracks={tracks}/>
+            }
+        </MediaQuery>
+    )
+);
