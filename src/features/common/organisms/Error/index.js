16f5bb7e9386f46baa6ead5985913debc9b8a23e
Shilza <andgalatinov@gmail.com>
2019-05-07 22:11:22 +0300

    Add Error component

---
+++ src/features/common/organisms/Error/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './styles.module.scss';
+
+export const Error = () => {
+    const reload = () => {
+        window.location.reload()
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1>Error</h1>
+            <span>
+            Something went wrong
+        </span>
+            <button className={styles.button} onClick={reload}>Try again</button>
+        </div>
+    );
+};
