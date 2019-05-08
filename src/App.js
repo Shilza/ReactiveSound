446068d70b764647cd972f74c250c436886ab330
Shilza <andgalatinov@gmail.com>
2019-05-08 18:05:13 +0300

    Add basepath in Router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {NoMatch} from "./features/common/";
 import {SongBar} from './features/common/organisms';
 
 const App = () => (
-    <Router>
+    <Router basename='ReactiveSound'>
         <Switch>
             {
                 routes.map(
