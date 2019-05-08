35605dbb8ee6f7b55686ef1e652266e346dbae5c
Shilza <andgalatinov@gmail.com>
2019-05-08 12:56:25 +0300

    Adaptive tracks component fix min width media query

--- src/ui/templates/AdaptiveTracks.js
+++ src/ui/templates/AdaptiveTracks.js
@@ -4,7 +4,7 @@ import {WaveformTracks} from "./WaveformTracks";
 import {Tracks} from "../../features/favorites/templates";
 
 export const AdaptiveTracks = React.memo(({tracks, hasMore, fetchNext}) => (
-        <MediaQuery minWidth={960}>
+        <MediaQuery minWidth={1000}>
             {(matches) =>
                 matches
                     ? <WaveformTracks hasMore={hasMore} fetchNext={fetchNext} tracks={tracks}/>
