93ac9b82635ac45ea4936fbde89f7d40ede74965
Shilza <andgalatinov@gmail.com>
2019-05-09 14:32:36 +0300

    Small UI fixes

--- src/features/common/molecules/Navigation/index.js
+++ src/features/common/molecules/Navigation/index.js
@@ -8,20 +8,20 @@ export const Navigation = ({onSearch}) => (
         <ul className={styles.iconsList}>
             <li>
                 <Button onClick={onSearch} id='searchButton'>
-                    <Icon id='searchIcon' name='search' width='1.4em' height='1.4em' viewBox='0 0 500 500'/>
+                    <Icon id='searchIcon' name='search' width='1.1em' height='1.1em' viewBox='0 0 500 500'/>
                 </Button>
             </li>
             <li>
                 <Button>
                     <a href='http://soundcloud.com/' rel='noopener noreferrer' target='_blank'>
-                        <Icon name='soundcloud' width='1.4em' height='1.4em' viewBox='0 0 100 100'/>
+                        <Icon name='soundcloud' width='1.1em' height='1.1em' viewBox='0 0 100 100'/>
                     </a>
                 </Button>
             </li>
             <li>
                 <Button>
                     <a href='https://github.com/Shilza/ReactiveSound' rel='noopener noreferrer' target='_blank'>
-                        <Icon name='github' width='1.4em' height='1.4em' viewBox='0 0 500 500'/>
+                        <Icon name='github' width='1.1em' height='1.1em' viewBox='0 0 500 500'/>
                     </a>
                 </Button>
             </li>
