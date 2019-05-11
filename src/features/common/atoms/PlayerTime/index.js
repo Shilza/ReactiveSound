c188d298041def9ff4ab317f9ebc35a555f3ead7
Shilza <andgalatinov@gmail.com>
2019-05-11 21:25:59 +0300

    Memoize duration

--- src/features/common/atoms/PlayerTime/index.js
+++ src/features/common/atoms/PlayerTime/index.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useMemo} from "react";
 import styles from './styles.module.scss';
 import {connect} from "react-redux";
 import {convertMsToString} from "../../utils";
 
-const PlayerTime = ({currentTime, duration}) => (
-    <div className={styles.container}>
-        <span>{convertMsToString(currentTime)}</span>
-        /
-        <span>{duration}</span>
-    </div>
-);
+const PlayerTime = ({currentTime, duration}) => {
+    let memoizedDuration = useMemo(() => convertMsToString(duration), [duration]);
+
+    return (
+        <div className={styles.container}>
+            <span>{convertMsToString(currentTime)}</span>
+            /
+            <span>{memoizedDuration}</span>
+        </div>
+    );
+};
 
 export default connect(state => ({
     currentTime: state.player.currentTime,
