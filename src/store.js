9bb2e36b921b7ac0db1e8e849cc869d2e8821022
Shilza <andgalatinov@gmail.com>
2019-05-17 22:10:01 +0300

    Add redux-mock-store to dependencies

--- src/store.js
+++ src/store.js
@@ -1,14 +1,8 @@
 import {applyMiddleware, createStore} from 'redux';
 import {RootReducer} from './reducers';
 import createSagaMiddleware from 'redux-saga';
-import {all} from 'redux-saga/effects';
-import {favoritesSagas} from "./features/favorites/sagas";
-import {trackInterval} from "./features/common/middlewares";
-import {seek} from "./features/common/middlewares";
-import {stopPlayer} from "./features/common/middlewares";
-import {searchSagas} from "./features/search/sagas";
-import {usersSagas} from "./features/users/sagas";
-import {playerSagas} from "./features/common/sagas";
+import {seek, stopPlayer, trackInterval} from "./features/common/middlewares";
+import {rootSaga} from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,15 +10,6 @@ const store = createStore(
     RootReducer,
     applyMiddleware(sagaMiddleware, trackInterval, seek, stopPlayer)
 );
-
-function* rootSaga() {
-    yield all([
-        ...favoritesSagas,
-        ...playerSagas,
-        ...searchSagas,
-        ...usersSagas
-    ])
-}
 
 sagaMiddleware.run(rootSaga);
 
