a963ff2829bd10b17968fe0a3a3b5f96b7ccbedd
Shilza <andgalatinov@gmail.com>
2019-05-18 23:08:31 +0300

    Code style fixes in common action creators

--- src/features/common/actionCreators/index.js
+++ src/features/common/actionCreators/index.js
@@ -9,26 +9,25 @@ import {
     FETCHED_PREVIOUS, FETCHED_WAVEFORM_TRACKS, REQUESTED_WAVEFORM_SUCCEED
 } from "../actionTypes";
 
-export const fetchTrack = payload => ({ type: FETCHED_TRACK, payload });
+export const fetchTrack = payload => ({type: FETCHED_TRACK, payload});
 
-export const setPlayer = payload => ({ type: SET_PLAYER, payload });
+export const setPlayer = payload => ({type: SET_PLAYER, payload});
 
-export const setPlayerInterval = payload => ({ type: SET_PLAYER_INTERVAL, payload });
+export const setPlayerInterval = payload => ({type: SET_PLAYER_INTERVAL, payload});
 
-export const setCurrentTime = payload => ({ type: SET_CURRENT_TIME, payload });
+export const setCurrentTime = payload => ({type: SET_CURRENT_TIME, payload});
 
 export const playTrack = () => ({type: PLAY_TRACK});
 
-export const pauseTrack = () => ({ type: PAUSE_TRACK });
+export const pauseTrack = () => ({type: PAUSE_TRACK});
 
-export const seekTo = payload => ({ type: SEEK_TO, payload });
+export const seekTo = payload => ({type: SEEK_TO, payload});
 
-export const fetchNext = payload => ({ type: FETCHED_NEXT, payload });
+export const fetchNext = payload => ({type: FETCHED_NEXT, payload});
 
-export const fetchPrevious = payload => ({ type: FETCHED_PREVIOUS, payload });
+export const fetchPrevious = payload => ({type: FETCHED_PREVIOUS, payload});
 
 
+export const fetchWaveform = payload => ({type: FETCHED_WAVEFORM_TRACKS, payload});
 
-export const fetchWaveform = payload => ({ type: FETCHED_WAVEFORM_TRACKS, payload });
-
-export const requestWaveformSuccess = payload => ({ type: REQUESTED_WAVEFORM_SUCCEED, payload });
+export const requestWaveformSuccess = payload => ({type: REQUESTED_WAVEFORM_SUCCEED, payload});
