002a987f39d26af08489a132062fdb4eac1e6137
Shilza <andgalatinov@gmail.com>
2019-05-27 23:21:25 +0300

    Add search feature tests

---
+++ src/features/search/molecules/Action/index.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {render} from 'react-testing-library';
+import {Action} from "./index";
+
+describe('Test search Action', () => {
+    afterEach(() => {});
+
+    const labelText = 'label';
+    const {container, getByTestId} = render(
+        <Action label={labelText}>
+            <div>Children</div>
+        </Action>
+    );
+
+    it('should renders correctly', () => {
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should have two child', () => {
+        expect(container.firstChild.childNodes.length).toBe(2);
+    });
+
+    it('should have correct label', () => {
+        expect(getByTestId('actionLabel')).toHaveTextContent(labelText);
+    });
+});
---
+++ src/features/search/organisms/TrackCard/index.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {render} from 'react-testing-library';
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import TrackCard from './index';
+import {BrowserRouter as Router} from "react-router-dom";
+
+describe('Test search TrackCard', () => {
+    const username = 'Username';
+    const id = 5;
+    const userId = 5;
+    const playbackCount = 100;
+    const link = 'link';
+    const title = 'title';
+    const duration = 200;
+    const likesCount = 200;
+
+    const mockStore = configureStore();
+    const store = mockStore({
+        player: {
+            player: {},
+            currentTrack: {
+                id: 5
+            },
+            trackIntervalId: 123
+        }
+    });
+
+    const {container, getByTestId} = render(
+        <Provider store={store}>
+            <Router>
+                <TrackCard
+                    username={username}
+                    id={id}
+                    userId={userId}
+                    playbackCount={playbackCount}
+                    link={link}
+                    title={title}
+                    duration={duration}
+                    likesCount={likesCount}
+                />
+            </Router>
+        </Provider>
+    );
+
+    it('should renders correctly', () => {
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should have correct user\'s link', () => {
+        const userLink = getByTestId('userLink');
+        expect(userLink).toHaveAttribute('href', `/users/${userId}/tracks`);
+    });
+
+    it('should have correct track link', () => {
+        const userLink = getByTestId('trackLink');
+        expect(userLink).toHaveAttribute('href', link);
+    });
+});
