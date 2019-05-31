104f01cf898aa5982d7540a1faf2082775e9d9b6
Shilza <andgalatinov@gmail.com>
2019-05-31 12:44:53 +0300

    Fix size of pause button

--- src/features/common/atoms/PlayButton/index.js
+++ src/features/common/atoms/PlayButton/index.js
@@ -10,7 +10,7 @@ export const PlayButton = withPlayer(({onPlay, isPlay}) => (
     >
         {
             isPlay ?
-                <Icon name='pause' viewBox='0 0 420 420' fill='#4d4e4f'/>
+                <Icon name='pause' viewBox='0 0 360 360' fill='#4d4e4f'/>
                 :
                 <Icon name='play' viewBox='0 0 420 420' fill='#4d4e4f'/>
         }
