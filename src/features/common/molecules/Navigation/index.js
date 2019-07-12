3a65d08fcc0c243d7bc0f4efff03cb9609dff4a8
Galatinov <andgalatinov@gmail.com>
2019-07-12 22:45:54 +0300

    Add aria-label for buttons

--- src/features/common/molecules/Navigation/index.js
+++ src/features/common/molecules/Navigation/index.js
@@ -7,19 +7,24 @@ export const Navigation = ({onSearch}) => (
     <nav className={styles.container}>
         <ul className={styles.iconsList}>
             <li>
-                <Button onClick={onSearch} id='searchButton' data-testid='searchButton'>
+                <Button
+                    onClick={onSearch}
+                    id='searchButton'
+                    aria-label='Search track'
+                    data-testid='searchButton'
+                >
                     <Icon id='searchIcon' name='search' width='1.1em' height='1.1em' viewBox='0 0 500 500'/>
                 </Button>
             </li>
             <li>
-                <Button>
+                <Button aria-label='SoundCloud link'>
                     <a href='http://soundcloud.com/' rel='noopener noreferrer' target='_blank'>
                         <Icon name='soundcloud' width='1.1em' height='1.1em' viewBox='0 0 100 100'/>
                     </a>
                 </Button>
             </li>
             <li>
-                <Button>
+                <Button aria-label='Github link'>
                     <a href='https://github.com/Shilza/ReactiveSound' rel='noopener noreferrer' target='_blank'>
                         <Icon name='github' width='1.1em' height='1.1em' viewBox='0 0 430 430'/>
                     </a>
